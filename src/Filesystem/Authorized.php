3f48f2664deafc75dd973d9d1bab30064c2173e2
taymaz <75164763+taymazmostafaei@users.noreply.github.com>
2022-12-31 15:22:39 +0330

    add media format list

---
+++ src/Filesystem/Authorized.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Taymaz\Mediabackup\Filesystem;
+
+trait Authorized
+{
+    public $Pictures = [
+        'jpg',
+        'jpeg',
+        'jfif',
+        'pjpeg',
+        'pjp',
+        'png',
+        'svg',
+        'webp',
+        'png',
+
+    ];
+
+    public $Audios = [
+        '3gp',
+        'aa',
+        'aac',
+        'aax',
+        'act',
+        'aiff',
+        'alac',
+        'amr',
+        'ape',
+        'au',
+        'awb',
+        'dss',
+        'dvf',
+        'flac',
+        'gsm',
+        'iklax',
+        'ivs',
+        'm4a',
+        'm4b',
+        'm4p',
+        'mmf',
+        'mp3',
+        'mpc',
+        'msv',
+        'nmf',
+        'mogg',
+        'ogg',
+        'oga',
+        'opus',
+        'ra',
+        'rm',
+        'raw',
+        'rf64',
+        'sln',
+        'tta',
+        'voc',
+        'vox',
+        'wav',
+        'wma',
+        'wv',
+        'webm',
+        '8svx',
+        'cda',
+
+    ];
+
+    public $Videos = [
+        'mp4',
+        'mov',
+        'wmv',
+        'avi',
+        'avchd',
+        'mkv',
+        'webm',
+        'flv',
+        '4av',
+        'swf',
+
+
+    ];
+}
