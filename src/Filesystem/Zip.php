bead2b6abc082426e714b5e84264caf86fb811cf
taymaz <75164763+taymazmostafaei@users.noreply.github.com>
2023-02-01 12:15:51 +0330

    add progress bar for zip part

--- src/Filesystem/Zip.php
+++ src/Filesystem/Zip.php
@@ -3,6 +3,8 @@
 namespace Taymaz\Mediabackup\Filesystem;
 
 use Taymaz\Mediabackup\ConfigLoader;
+use DivineOmega\CliProgressBar\ProgressBar;
+
 use ZipArchive;
 
 trait Zip
@@ -10,21 +12,29 @@ trait Zip
     public function createZip($files, ConfigLoader $config)
     {
 
+        $max = sizeof($files);
+
+        $progressBar = new ProgressBar;
+        $progressBar->setMaxProgress($max);
+
+
         $zip = new ZipArchive;
-        $bacupZipFile = 'backup-'. date('Y-m-d') .'.zip';
+        $bacupZipFile = 'backup-' . date('Y-m-d') . '.zip';
         $BackupExportPath = $config->configs->BackupExportPath;
-        if ($zip->open($BackupExportPath == '.' ? $bacupZipFile : $BackupExportPath . '/' . $bacupZipFile , ZipArchive::CREATE) === TRUE) {
+        if ($zip->open($BackupExportPath == '.' ? $bacupZipFile : $BackupExportPath . '/' . $bacupZipFile, ZipArchive::CREATE) === TRUE) {
 
             foreach ($files as $file) {
+
+                $progressBar->advance()->display();
                 $zip->addFile($file, $file);
             }
 
             $zipFilePath = $zip->filename;
             $zip->close();
 
+            $progressBar->complete();
+
             return $zipFilePath;
         }
-
-
     }
 }
